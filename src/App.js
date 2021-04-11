340439bbcaf2ea7b999226066f79059aa4823008
unknown <tompsherman@gmail.com>
2021-04-11 09:35:04 -0400

    app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 import './App.css'
 import Grid from './components/Grid'
+import { GridContext } from "./components/context/GridContext"
 
 const App = () => {
+  const [ startingGrid, setStartingGrid ] = useState()
+
+  useEffect(()=>{
+    console.log("starting grid in App", startingGrid)
+  }, [startingGrid])
+
   return (
-    <div className="App">
-
-      <h2>Conway's Game of Life!</h2>
-      <Grid />
-      <button>start</button>
-      <button>stop</button>
-      <button>clear</button>
-      <h4>Generation Number: ____</h4>
-
-    </div>
+    <GridContext.Provider value={{ startingGrid, setStartingGrid }}>
+      <div className="App">
+  
+        <h2>Conway's Game of Life!</h2>
+        <Grid />
+        <button>start</button>
+        <button>stop</button>
+        <button>clear</button>
+        <h4>Generation Number: ____</h4>
+  
+      </div>
+    </GridContext.Provider>
   );
 };
 
